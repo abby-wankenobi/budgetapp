ad1d51fd4743eba136346b56172814c9280d0647
Abigail Swarth <abigailswarth@gmail.com>
2018-07-16 15:26:02 -0400

    formatting numbers

--- app.js
+++ app.js
@@ -150,7 +150,23 @@ var UIController = (function() {
       percentageLabel: '.budget__expenses--percentage',
       container: '.container',
       expensesPercentageLabel: '.item__percentage'
-    }
+    };
+
+    var formatNumber = function(num, type) {
+      var numSplit, int, dec;
+
+      num = Math.abs(num);
+      num = num.toFixed(2);
+      numSplit = num.split('.')
+
+      int = numSplit[0];
+      if(int.length > 3) {
+        int = int.substr(0, int.length - 3) + ',' + int.substr(int.length-3, 3);
+      }
+      dec = numSplit[1];
+
+      return (type === 'expense' ? '-' : '+') + ' ' + int + '.' + dec;
+    };
 
     return {
       getInput: function() {
@@ -176,7 +192,7 @@ var UIController = (function() {
         //replace placeholder text with data
         newHtml = html.replace('%id%', obj.id);
         newHtml = newHtml.replace('%description%', obj.description);
-        newHtml = newHtml.replace('%value%', obj.value);
+        newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
         //insert html into DOM
         document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -204,9 +220,11 @@ var UIController = (function() {
       },
 
       displayBudget: function(obj){
-        document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-        document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-        document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+        obj.budget > 0 ? type = 'income' : type = 'expense'
+        document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+        document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'income');
+        document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'expense');
 
 
         if(obj.percentage > 0) {
