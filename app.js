27fa53150013acd9a323b7f6411c1ee43b506661
Abigail Swarth <abigailswarth@gmail.com>
2018-07-16 14:35:31 -0400

    UI percentages

--- app.js
+++ app.js
@@ -148,7 +148,8 @@ var UIController = (function() {
       incomeLabel: '.budget__income--value',
       expensesLabel: '.budget__expenses--value',
       percentageLabel: '.budget__expenses--percentage',
-      container: '.container'
+      container: '.container',
+      expensesPercentageLabel: '.item__percentage'
     }
 
     return {
@@ -215,6 +216,25 @@ var UIController = (function() {
         }
       },
 
+      displayPercentages: function(percentages) {
+        var fields = document.querySelectorAll(DOMstrings.expensesPercentageLabel);
+
+        var nodeListForEach = function(list, callback) {
+          for (var i = 0; i < list.length; i++){
+            callback(list[i], i)
+          }
+        }
+
+        nodeListForEach(fields, function(current, index) {
+          if(percentages[index] > 0) {
+            current.textContent = percentages[index] + "%";
+          } else {
+            current.textContent = "--";
+          }
+        });
+
+      },
+
       getDOMstrings: function() {
         return DOMstrings;
       }
@@ -258,7 +278,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var percentages = budgetCtrl.getPercentages();
 
     //3. update UI
-    console.log(percentages)
+    UICtrl.displayPercentages(percentages);
   };
 
   var ctrlAddItem = function() {
