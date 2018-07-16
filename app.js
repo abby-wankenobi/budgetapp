5941319a28aad91695ab3f7fa0c8b981d29a272a
Abigail Swarth <abigailswarth@gmail.com>
2018-07-16 15:31:54 -0400

    display date

--- app.js
+++ app.js
@@ -149,7 +149,8 @@ var UIController = (function() {
       expensesLabel: '.budget__expenses--value',
       percentageLabel: '.budget__expenses--percentage',
       container: '.container',
-      expensesPercentageLabel: '.item__percentage'
+      expensesPercentageLabel: '.item__percentage',
+      dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -253,6 +254,15 @@ var UIController = (function() {
 
       },
 
+      displayMonth: function() {
+        var now, months, year, month;
+        now = new Date();
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        month = now.getMonth();
+        year = now.getFullYear();
+        document.querySelector(DOMstrings.dateLabel).textContent = months[month] + " " + year;
+      },
+
       getDOMstrings: function() {
         return DOMstrings;
       }
@@ -349,6 +359,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log('begin')
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
